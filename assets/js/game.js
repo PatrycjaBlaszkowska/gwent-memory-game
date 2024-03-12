242a0ef6b4f48cad15d9399975e0fb70df10600d
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-03-12 13:14:45 +0000

    Added selectors(timer,score,flips) and a data attribute to each card in order to build checkForMatch() function.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,35 @@
+
+let selectors = {
+  timer: document.querySelector("#timer"),
+  score: document.querySelector("#score"),
+  flips: document.querySelector("#flips")
+}
+
 const cards = document.querySelectorAll('.game-card');
 
+let firstCard, secondCard;
+let hasFlipped = false;
+
+
+/**
+ * This function flips a card and 
+ * change hasFlipped to either right or false
+ * @returns card and calling check checkForMatch() function.
+ */
+
 function flipCard() {
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+
+  if (!hasFlipped) {
+    hasFlipped = true;
+    firstCard = this;
+    return 
+  }
+
+  secondCard = this;
+  hasFlipped = false;
+
+  checkForMatch();
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
