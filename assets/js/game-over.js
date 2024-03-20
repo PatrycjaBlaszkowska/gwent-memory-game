24fd66a49f4515d205452132c121c6b4cc4eaedc
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-03-20 23:17:45 +0000

    Added a leaderboard.html to store best scores.

--- assets/js/game-over.js
+++ assets/js/game-over.js
@@ -3,6 +3,8 @@
 const username = document.querySelector("#username");
 const currentScore = localStorage.getItem("currentScore");
 const finalScore = document.querySelector("#finalScore");
+const bestScores = JSON.parse(localStorage.getItem("bestScores")) || [];
+const leaderboard = document.querySelector("#leaderboard");
 
 finalScore.innerText = currentScore;
 
@@ -13,5 +15,12 @@ const saveScore = e => {
         score: currentScore,
         name: username.value
     };
-}
+
+    const scoreEntry = document.createElement("li");
+    scoreEntry.innerText = `${score.name}: ${score.score}`;
+
+    leaderboard.appendChild(scoreEntry);
+};
+
+
 
