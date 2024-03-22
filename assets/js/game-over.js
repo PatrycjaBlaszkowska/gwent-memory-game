e36f8512d5228059131a5957737cc0bc2d04b085
Patrycja Blaszkowska <pblaszkowska@icloud.com>
2024-03-22 13:24:49 +0000

    Fixed validation for a username input in game over screen.

--- assets/js/game-over.js
+++ assets/js/game-over.js
@@ -3,19 +3,19 @@
 const username = document.querySelector("#username");
 const currentScore = localStorage.getItem("currentScore");
 const finalScore = document.querySelector("#finalScore");
+const saveButton = document.querySelector("#save-score-btn");
 const bestScores = JSON.parse(localStorage.getItem("bestScores")) || [];
-
-const MAX_BEST_SCORES = 10;
-
 
 finalScore.innerText = currentScore;
 
+username.addEventListener("keyup", () => {
+    saveButton.disabled = !username.value;
+});
 /**
  * This function takes user input(name)
  *  push and sort score to the local
  * storage for the feature use.
  */
-
 const saveScore = e => {
     e.preventDefault();
 
@@ -30,7 +30,7 @@ const saveScore = e => {
     bestScores.splice(10);
 
     localStorage.setItem("bestScores", JSON.stringify(bestScores));
-    window.location.assign("game-over.html");
+    window.location.assign("leaderboard.html");
 };
 
 
